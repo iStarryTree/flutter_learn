78bf54a344d84d2e2ee44f72d8d2044486d71f6a
starry_tree <starry_tree@outlook.com>
2024-01-31 22:17:55 +0800

    init

---
+++ official_document/navigation_routin/send_data_navigation/README.md
@@ -0,0 +1,3 @@
+# send_data_navigation
+
+A new Flutter project.
---
+++ official_document/navigation_routin/send_data_navigation/android/app/src/main/kotlin/com/example/send_data_navigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.send_data_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
