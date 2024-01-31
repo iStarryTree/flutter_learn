551962c47a28e4fa2d13905f2687ce4d4fae1693
starry_tree <starry_tree@outlook.com>
2024-01-31 22:34:45 +0800

    route_settings

--- official_document/navigation_routin/send_data_navigation/lib/main.dart
+++ official_document/navigation_routin/send_data_navigation/lib/main.dart
@@ -22,7 +22,9 @@ class MainApp extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => DetailScreen(todo: todos[index]),
+                    // builder: (context) => DetailScreen(todo: todos[index]),
+                    builder: (context) => const DetailScreen(),
+                    settings: RouteSettings(arguments: todos[index]),
                   ),
                 );
               },
@@ -47,12 +49,15 @@ final todos = List.generate(
 );
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key, required this.todo});
+  // const DetailScreen({super.key, required this.todo});
+  const DetailScreen({super.key});
 
-  final Todo todo;
+  // final Todo todo;
 
   @override
   Widget build(BuildContext context) {
+    final todo = ModalRoute.of(context)!.settings.arguments as Todo;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(todo.title),
