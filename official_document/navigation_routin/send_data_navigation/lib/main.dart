b1826c66188e94578885e417ccf0bde758ad15f1
starry_tree <starry_tree@outlook.com>
2024-01-31 22:27:49 +0800

    navigation data

--- official_document/navigation_routin/send_data_navigation/lib/main.dart
+++ official_document/navigation_routin/send_data_navigation/lib/main.dart
@@ -9,11 +9,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Todo App'),
         ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(todos[index].title),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(todo: todos[index]),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+final todos = List.generate(
+  20,
+  (index) => Todo('Todo $index', 'Description of todo $index'),
+);
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key, required this.todo});
+
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(todo.description),
       ),
     );
   }
